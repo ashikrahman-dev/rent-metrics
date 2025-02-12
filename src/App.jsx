8aaeb6059c210205726e6a93d791c72102855358
ashikrahman-dev <ashikur1993@gmail.com>
2025-02-12 18:43:53 +0600

    Project Initialize

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <>
+            <h1>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi molestias explicabo soluta eaque dicta asperiores, et
+                sapiente voluptas. Fugiat suscipit repudiandae neque ea in cum
+                odio eligendi, officia id architecto!
+            </h1>
+        </>
+    );
+}
+
+export default App;
