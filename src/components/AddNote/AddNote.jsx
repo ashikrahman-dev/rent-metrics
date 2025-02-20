bd860f8ae07a2f109e4abd2cdacb74ace268ec5c
ashikrahman-dev <ashikur1993@gmail.com>
2025-02-20 20:44:26 +0600

    Update

--- src/components/AddNote/AddNote.jsx
+++ src/components/AddNote/AddNote.jsx
@@ -1,13 +1,6 @@
 import { useState } from "react";
 import { Button } from "../../components/ui/button";
 import { Label } from "../../components/ui/label";
-// import {
-//     Select,
-//     SelectContent,
-//     SelectItem,
-//     SelectTrigger,
-//     SelectValue,
-// } from "../../components/ui/select";
 
 import { Textarea } from "../../components/ui/textarea";
 import {
