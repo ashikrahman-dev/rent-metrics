411fcd9c6d5f3fccffc50126fa906fe4fd37df92
ashikrahman-dev <ashikur1993@gmail.com>
2025-03-12 17:20:59 +0600

    Update

--- src/pages/BusinessIntelligence/BusinessIntelligence.jsx
+++ src/pages/BusinessIntelligence/BusinessIntelligence.jsx
@@ -1,5 +1,8 @@
 import actionableInsights from "../../assets/images/actionable-insights-img.svg";
 import fasterReporting from "../../assets/images/better-data-faster-img.svg";
+import empowerInnovationIcon1 from "../../assets/images/empower-innovation-icon-1.svg";
+import empowerInnovationIcon2 from "../../assets/images/empower-innovation-icon-2.svg";
+import empowerInnovationIcon3 from "../../assets/images/empower-innovation-icon-3.svg";
 import rentlyticsIcon1 from "../../assets/images/rentlytics-icon-1.svg";
 import rentlyticsIcon2 from "../../assets/images/rentlytics-icon-2.svg";
 import rentlyticsIcon3 from "../../assets/images/rentlytics-icon-3.svg";
@@ -8,6 +11,7 @@ import rentlyticsIcon5 from "../../assets/images/rentlytics-icon-5.svg";
 import rentlyticsIcon6 from "../../assets/images/rentlytics-icon-6.svg";
 
 import BusinessIntelligenceBanner from "../../components/BusinessIntelligenceBanner/BusinessIntelligenceBanner";
+import Cta from "../../components/Cta/Cta";
 
 const rentlyticsData = [
     {
@@ -51,6 +55,29 @@ const rentlyticsData = [
         icon: rentlyticsIcon6,
         description:
             "Uncover hidden insights through filtering, sorting, highlighting, and drill-down functionalities. Quickly distinguish valuable signals from data noise to pinpoint potential property issues.",
+    },
+];
+const empowerInnovationData = [
+    {
+        id: 1,
+        title: "Ingest From Multiple Data Sources",
+        icon: empowerInnovationIcon1,
+        description:
+            "Get full visibility into all your properties–even if the data lives in multiple systems, or is managed across multiple teams. With a comprehensive, centralized view of your entire portfolio, it’s easy to spot trends, or see how day-to-day activities impact returns.",
+    },
+    {
+        id: 2,
+        title: "Constant Innovation",
+        icon: empowerInnovationIcon2,
+        description:
+            "Replace manual, error-prone data-aggregation and spreadsheets with intuitive dashboards for every role–from Operations, to Finance, to Marketing. Easy-to-use scorecards help you drive accountability, while keeping your team focused on what’s important.",
+    },
+    {
+        id: 3,
+        title: "Agility In The Cloud",
+        icon: empowerInnovationIcon3,
+        description:
+            "Seasonal changes, poor performing properties, increased utility costs and variances in occupancy rates can all be indicators that your strategy needs to change. Get to the root of the issue fast so you can solve problems, and maintain control of your bottom line.",
     },
 ];
 
@@ -145,6 +172,47 @@ export default function BusinessIntelligence() {
                     </div>
                 </div>
             </section>
+
+            <section className="xl:py-[120px] lg:py-[100px] md:py-20 sm:py-16 py-14 bg-main-shade">
+                <div className="container mx-auto md:px-0 px-4">
+                    <div className="text-center">
+                        <h2 className="xl:max-w-[705px] lg:max-w-[600px] max-w-[500px] mx-auto 2xl:text-[40px] xl:text-4xl lg:text-3xl sm:text-2xl text-xl leading-[1.3] font-semibold text-dark-1 text-center mb-4">
+                            Empower Innovation With Industry-Leading Technology
+                        </h2>
+                        <p className="xl:text-lg md:text-base text-sm text-dark-2 pb-8 max-w-[700px] mx-auto">
+                            Rentlytics is the only company that’s 100% focused
+                            on bringing innovative data analytics solutions to
+                            the multifamily real estate industry.
+                        </p>
+                        <div className="relative before:absolute before:content-[''] before:w-7.5 before:h-[3px] before:bg-main before:rounded-[30px] before:left-[calc(50%-15px)] before:bottom-0"></div>
+                    </div>
+                    {/* Heading end */}
+                    <div className="flex flex-col xl:gap-[100px] lg:gap-20 md:gap-16 gap-12 lg:mt-[60px] mt-20">
+                        <div className="grid grid-cols-12 sm:gap-6 gap-7.5 items-center">
+                            {empowerInnovationData.map((item) => (
+                                <div
+                                    key={item.id}
+                                    className="col-span-4 bg-white p-6 rounded-2xl shadow-[0px_32px_32px_0px_rgba(11,25,44,0.06)] h-full"
+                                >
+                                    <img
+                                        src={item.icon}
+                                        alt="icon"
+                                        className="max-w-full"
+                                    />
+                                    <h4 className="mt-6 text-2xl text-dark-1 font-bold leading-[1.3] mb-3">
+                                        {item.title}
+                                    </h4>
+                                    <p className="text-dark-2 text-lg leading-[1.4]">
+                                        {item.description}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <Cta />
         </>
     );
 }
