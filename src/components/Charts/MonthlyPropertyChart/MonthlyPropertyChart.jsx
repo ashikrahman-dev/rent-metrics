ccce9050e165ef3d97abd87acf6d9ede640e55c4
ashikrahman-dev <ashikur1993@gmail.com>
2025-03-17 10:31:21 +0600

    Update monthly performance chart

--- src/components/Charts/MonthlyPropertyChart/MonthlyPropertyChart.jsx
+++ src/components/Charts/MonthlyPropertyChart/MonthlyPropertyChart.jsx
@@ -9,18 +9,18 @@ import {
 } from "recharts";
 
 const data = [
-    { name: "Jan-24", sales: 45, revenue: 105, profit: 150 },
-    { name: "Feb-24", sales: 48, revenue: 175, profit: 100 },
-    { name: "Mar-24", sales: 50, revenue: 160, profit: 200 },
-    { name: "Apr-24", sales: 47, revenue: 165, profit: 250 },
-    { name: "May-24", sales: 52, revenue: 230, profit: 250 },
-    { name: "Jun-24", sales: 54, revenue: 190, profit: 250 },
-    { name: "July-24", sales: 52, revenue: 200, profit: 250 },
-    { name: "Aug-24", sales: 50, revenue: 190, profit: 250 },
-    { name: "Sep-24", sales: 51, revenue: 148, profit: 250 },
-    { name: "Oct-24", sales: 49, revenue: 173, profit: 250 },
-    { name: "Nov-24", sales: 50, revenue: 190, profit: 250 },
-    { name: "Dec-24", sales: 48, revenue: 165, profit: 250 },
+    { name: "Jan-24", sales: 5, revenue: 0, profit: 90 },
+    { name: "Feb-24", sales: 84, revenue: 175, profit: 100 },
+    { name: "Mar-24", sales: 160, revenue: 160, profit: 200 },
+    { name: "Apr-24", sales: 127, revenue: 165, profit: 150 },
+    { name: "May-24", sales: 72, revenue: 230, profit: 110 },
+    { name: "Jun-24", sales: 124, revenue: 190, profit: 180 },
+    { name: "July-24", sales: 152, revenue: 200, profit: 90 },
+    { name: "Aug-24", sales: 150, revenue: 190, profit: 148 },
+    { name: "Sep-24", sales: 151, revenue: 148, profit: 120 },
+    { name: "Oct-24", sales: 110, revenue: 173, profit: 140 },
+    { name: "Nov-24", sales: 140, revenue: 190, profit: 190 },
+    { name: "Dec-24", sales: 148, revenue: 165, profit: 50 },
 ];
 
 export default function MonthlyPropertyChart() {
@@ -30,7 +30,7 @@ export default function MonthlyPropertyChart() {
                 Monthly Performance
             </h2>
             <ResponsiveContainer width="100%" height={300}>
-                <LineChart data={data}>
+                <LineChart accessibilityLayer data={data}>
                     <CartesianGrid
                         strokeDasharray="-1 3"
                         stroke="#E9EDF0"
@@ -39,6 +39,8 @@ export default function MonthlyPropertyChart() {
                     />
                     <XAxis
                         dataKey="name"
+                        tickLine={false}
+                        axisLine={false}
                         tick={{
                             fontSize: 10,
                             fontWeight: 600,
@@ -66,11 +68,11 @@ export default function MonthlyPropertyChart() {
                         }}
                     />
                     <Line
-                        type="monotone"
+                        type="linear"
                         dataKey="revenue"
                         stroke="#2970CC"
-                        strokeWidth={2}
-                        dot={{ fill: "#2970CC", r: 2.5 }}
+                        strokeWidth={1}
+                        dot={false}
                         tick={{
                             fontSize: 10,
                             fontWeight: 600,
@@ -78,11 +80,28 @@ export default function MonthlyPropertyChart() {
                         }}
                     />
                     <Line
-                        type="monotone"
+                        type="linear"
+                        dataKey="profit"
+                        stroke="#68C8F8"
+                        strokeWidth={1}
+                        dot={false}
+                        tick={{
+                            fontSize: 10,
+                            fontWeight: 600,
+                            fill: "#54616B",
+                        }}
+                    />
+                    <Line
+                        type="linear"
                         dataKey="sales"
                         stroke="#30D287"
-                        strokeWidth={2}
-                        dot={{ fill: "#30D287", r: 2.5 }}
+                        strokeWidth={1}
+                        dot={false}
+                        tick={{
+                            fontSize: 10,
+                            fontWeight: 600,
+                            fill: "#54616B",
+                        }}
                     />
                 </LineChart>
             </ResponsiveContainer>
